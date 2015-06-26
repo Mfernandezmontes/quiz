aa198bf30205be40cc6bc143b36e68e5021dda51
adminlocal <mfernandezmontes@gmail.com>
2015-06-26 23:24:15 +0200

    -Creamos controlador quiz -Creamos vistas answer,question

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quiz_controller')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //    vista INDEX
   res.render('index', { title:  'QUIZ' });
 });
+
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
