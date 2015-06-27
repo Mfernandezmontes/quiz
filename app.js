097e8f3cac6309489ae0bb4cb80408809c46b985
adminlocal <mfernandezmontes@gmail.com>
2015-06-27 16:45:12 +0200

    -Rehacemos rutas -Rehacemos vistas -añadimos bootstrap

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials = require('express-partials')
 
 var routes = require('./routes/index');
 
@@ -21,6 +22,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+//view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(partials());
 
 //rutas
 app.use('/', routes); //index
