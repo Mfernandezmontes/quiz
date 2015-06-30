b5496bdd01c16c3f964fccb53d0d456f2b67891e
adminlocal <mfernandezmontes@gmail.com>
2015-07-01 00:20:33 +0200

    hotfix realizado

--- models/models.js
+++ models/models.js
@@ -3,7 +3,6 @@
  */
 var path = require('path');
 
-// TODO: mirar bien el problema de las variables de entrono
 var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/)
 var DB_name   = (url[6]||null);
 var user      = (url[2]||null);
@@ -12,7 +11,7 @@ var protocol =  (url[1]||null);
 var dialect   = (url[1]||null);
 var port      = (url[5]||null);
 var host      = (url[4]||null);
-//var storage   = process.env.DATABASE_STORAGE; //solo para sqlite
+var storage   = process.env.DATABASE_STORAGE; //solo para sqlite
 
 
 //Carga el modelo ORM
@@ -23,6 +22,7 @@ var sequelize = new Sequelize(DB_name, user,pwd,
         protocol : protocol,
         port : port,
         host : host,
+        storage : storage,
         omitNull : true
     })
 
@@ -35,7 +35,7 @@ exports.Quiz = Quiz; //Exporta la definicion
 //sequelize.sync() crea e inicializa la table de preguntas en DB
 sequelize.sync().then(function(){
     // sucess() ejecuta el manejador una vez creada la tabla
-    Quiz.count.then(function(count){
+    Quiz.count().then(function(count){
         if(count === 0){ // la tabla se inicializa solo si esta vacia
             Quiz.create({ pregunta: 'Capital de Italia',
                           respuesta: 'Roma'
